5c95e475e4e65f40ac5f3817b9c1d8c800f8cebf
Felix Jung <felix@jung.fm>
2015-05-11 10:34:38 +0200

    Updated CORS

--- server/routes/search.js
+++ server/routes/search.js
@@ -7,7 +7,8 @@ router.route('/:tag')
   // Add headers
   .get(function (req, res, next) {
      res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     res.header("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept");
      res.header('Access-Control-Allow-Headers', 'Content-Type');
      next();
   })
