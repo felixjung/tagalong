afaa503cf1ea76ccff8d0dbeb7db027f36627e2e
Felix Jung <felix@jung.fm>
2015-05-11 10:36:10 +0200

    Removed duplication in CORS setup

--- server/routes/search.js
+++ server/routes/search.js
@@ -9,7 +9,6 @@ router.route('/:tag')
      res.header("Access-Control-Allow-Origin", "*");
      res.header("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept");
-     res.header('Access-Control-Allow-Headers', 'Content-Type');
      next();
   })
   .get(function(req, res, next) {
